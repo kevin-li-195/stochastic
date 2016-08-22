5a1a8317b1b9dafd76baf0c0fc6ae60c42b82bb5
Kevin Li <kevin.li195@hotmail.com>
2016-08-21 23:55:39 -0400

    Add tests

---
+++ test/Normal3.hs
@@ -0,0 +1,21 @@
+import Data.Sample
+import Data.Sample.Types
+import Data.Sample.Lib
+
+import System.Random
+
+normalInit :: MonteCarlo
+normalInit = normalMC 10 3
+
+f :: Double -> MonteCarlo
+f d = if d < 7 then normalMC (0.9 * d) (0.3 * d)
+  else if d > 13 then normalMC (1.1 * d) (0.3 * d)
+  else normalMC d (0.3 * d)
+
+normal3 :: MonteCarlo
+normal3 = composeMC 3 normalInit f
+
+main = do
+    gen <- getStdGen
+    print "normal3"
+    print $ runMCN 10000 normal3 gen
